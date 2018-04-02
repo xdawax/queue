2a87abfd6bc29a3b1da63cc18933dcbac45c72c1
xdawax <david.carlsson.6652@student.uu.se>
2018-04-02 15:19:27 +0200

    First commit

---
+++ Queue/cmake-build-debug/CMakeFiles/Queue.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Queue.dir/main.c.o"
+  "CMakeFiles/Queue.dir/queue.c.o"
+  "CMakeFiles/Queue.dir/queue_test.c.o"
+  "Queue.pdb"
+  "Queue"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Queue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
