c145ec66fbdcda15a218aadcdec9188e047d91c8
xdawax <david.carlsson.6652@student.uu.se>
2018-04-02 16:18:26 +0200

    Added CUnit tests

--- queue.h
+++ queue.h
@@ -52,7 +52,7 @@ bool queue_pop(queue_t *queue);
 /// \returns: void
 void queue_display(queue_t *queue);
 
-/// \returns: the first link in the queuea
+/// \returns: the first link in the queue
 link_t* queue_get_first(queue_t *queue);
 
 /// \returns: the last link in the queue
